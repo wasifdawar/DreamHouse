ee740a7d8493bb8bfe0d7c38c8ddd1dc00439854
Wasif Dawar <m.wasifdawar@gmail.com>
2023-10-20 02:17:36 +0500

    Comment Added

--- force-app/main/default/lwc/housingMap/housingMap.js
+++ force-app/main/default/lwc/housingMap/housingMap.js
@@ -1,15 +1,27 @@
-import { LightningElement, wire } from 'lwc';
-import getHouses from "@salesforce/apex/HouseService.getRecords"
-export default class HousingMap extends LightningElement 
-{
-mapMakers;
-error;
-@wire(getHouses)
-wiredHouses({error,data})
-{
-    if(data)
-    {
-        
+import { LightningElement, wire } from "lwc";
+import getHouses from "@salesforce/apex/HouseService.getRecords";
+export default class HousingMap extends LightningElement {
+    //for git commit
+    mapMarkers;
+    error;
+    @wire(getHouses)
+    wiredHouses({ error, data }) {
+        if (data) {
+    // We are using Javascript Map function to transform the
+      this.mapMarkers = data.map((element) => {
+        return {
+          location: {
+            Street: element.Address__c,
+            City: element.City__c,
+            State: element.State__c
+          },
+          title: element.Name
+        };
+      });
+      this.error = undefined;
+    } else if (error) {
+      this.error = error;
+      this.mapMarkers = undefined; 
     }
-}
+  }
 }
